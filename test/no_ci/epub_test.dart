9e207d298a38284cb987c9d8083a16cafb5a4b44
dev2-nomo <dev2@nomo.app>
2023-11-08 12:04:03 +0100

    New Test for quickly searching a given EPubKey

---
+++ test/no_ci/epub_test.dart
@@ -0,0 +1,23 @@
+import 'package:test/test.dart';
+import 'package:walletkit_dart/walletkit_dart.dart';
+
+import '../ci/fetching/fetch_utxo_transactions_test.dart';
+
+void main() {
+  ///
+  /// Fetches all Transactions and calculates the balance for a given
+  ///
+  test('Fetch Extended Public Key Wallet', () async {
+    final epubKey =
+        "xpub692ymxrAm8ZiquZW4AwHmXg8xnSsFeVyzGGTRBBmKVhRvYqwGjfd5yYw8JToejJJrKwKexFXgjnr4thKaSqYBSXKZvuxQtUDHRZsYoVaE8p";
+
+    final (txs, _) = await fetchUTXOTransactions(
+      walletTypes: [HDWalletType.NO_STRUCTURE],
+      addressTypes: [AddressType.legacy],
+      networkType: ZeniqNetwork,
+      ePubKey: epubKey,
+    );
+
+    reportCoinsAndAddresses(txList: txs, type: ZeniqNetwork);
+  });
+}
