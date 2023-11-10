49c6efe889514ddc945c68e9a7cac048a4cdcea2
nomo-fe <dev0@nomo.app>
2023-11-10 16:54:41 +0100

    expect the results of getTransactionFromMessageHash

--- test/ci/parsing/parse_hex_transaction_test.dart
+++ test/ci/parsing/parse_hex_transaction_test.dart
@@ -3,7 +3,6 @@ import 'package:convert/convert.dart';
 import 'package:test/test.dart';
 import 'package:walletkit_dart/src/crypto/evm/rlp.dart';
 import 'package:web3dart/web3dart.dart';
-import 'package:walletkit_dart/src/utils/bigint_utils.dart';
 
 void main() {
   test('parse hex transaction', () {
@@ -16,29 +15,20 @@ void main() {
       ),
     );
 
-    DecodedRLP en = decodeRLP(message, 0);
+    final DecodedRLP en = decodeRLP(message, 0);
 
-    BigInt nonceFromTx = parseAsHexBigInt(en.result[0]);
-    BigInt gasPriceFromTx = parseAsHexBigInt(en.result[1]);
-    BigInt gasLimitFromTx = parseAsHexBigInt(en.result[2]);
-    String evmAddressFromTx = en.result[3];
-    BigInt valueFromTx = parseAsHexBigInt(en.result[4]);
+    final Uint8List data = Uint8List.fromList(hex.decode(en.result[6]));
 
-    Uint8List data = Uint8List.fromList(hex.decode(en.result[6]));
-
-    String functionSignature = hex.encode(data.sublist(0, 4));
-
-    if (functionSignature == "59454e49") {
-      print("ERC 20 Token detected!");
-    }
+    final String functionSignature = hex.encode(data.sublist(0, 4));
+    expect(functionSignature, "59454e49"); // ZEN20
 
     final Transaction tx = getTransactionFromMessageHash(unsignedTxFromNomo);
 
-    expect(nonceFromTx, BigInt.from(1864));
-    expect(gasPriceFromTx, BigInt.from(10000000000));
-    expect(gasLimitFromTx, BigInt.from(21000));
-    expect(evmAddressFromTx,
-        "05870f1507d820212E921e1f39f14660336231D1".toLowerCase());
-    expect(valueFromTx, BigInt.from(100000000000000000));
+    expect(tx.nonce, 1864);
+    expect(tx.gasPrice!.getInWei, BigInt.from(10000000000));
+    expect(tx.maxGas, 21000);
+    expect(tx.to!.hex, "0x05870f1507d820212e921e1f39f14660336231d1");
+    expect(tx.value!.getInWei, BigInt.from(100000000000000000));
+    //expect(tx.data, ""); // TODO: parse data
   });
 }
