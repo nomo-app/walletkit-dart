e2fbac3e403d7c5d7a35ba77fd96dc00084da0ba
dev2-nomo <dev2@nomo.app>
2023-11-10 11:20:18 +0100

    custom rlp decoding

---
+++ lib/src/crypto/evm/rlp.dart
@@ -0,0 +1,135 @@
+import 'dart:typed_data';
+
+import 'package:convert/convert.dart';
+import 'package:web3dart/web3dart.dart';
+
+class DecodedRLP {
+  final int consumed;
+  final dynamic result;
+
+  DecodedRLP({required this.consumed, required this.result});
+}
+
+/**
+ * 
+ * @param {Uint8List} data
+ * @param {int} offset
+ * @param {int} length
+ * 
+ * This function iterates over the data and returns the integer value of the data
+ * 
+ * @returns {int}
+ */
+int unarrayifyInteger(Uint8List data, int offset, int length) {
+  int result = 0;
+  for (int i = 0; i < length; i++) {
+    result = (result * 256) + data[offset + i];
+  }
+  return result;
+}
+
+/**
+ * 
+ * @param {int} value
+ * 
+ * This function takes an integer value, converts it to a hexadecimal string, 
+ * and ensures that the string is at least two characters long by padding with zeros 
+ * if necessary.
+ * 
+ * @returns {String}  The result is prefixed with "0x" 
+ */
+String hexlifyByte(int value) {
+  String result = value.toRadixString(16);
+  while (result.length < 2) {
+    result = "0" + result;
+  }
+
+  return "0x" + result;
+}
+
+/**
+ * @param {Uint8List} data
+ * @param {int} offset
+ * 
+ * This function is the main RLP decoding logic. It recursively decodes RLP-encoded data.
+  It checks the first byte of the data to determine the type of encoding (long item, list, long string, short string).
+  Depending on the type, it calls _decodeChildren to handle the decoding recursively.
+  The decoded result is stored in a List<dynamic>.
+  The function returns a Decoded object containing the consumed bytes and the result.
+ * 
+ */
+DecodedRLP decodeRLP(Uint8List data, int offset) {
+  DecodedRLP _decodeChildren(
+      Uint8List data, int offset, int childOffset, int length) {
+    List<dynamic> result = [];
+
+    while (childOffset < offset + 1 + length) {
+      DecodedRLP decoded = decodeRLP(data, childOffset);
+
+      result.add(decoded.result);
+
+      childOffset += decoded.consumed;
+    }
+
+    return DecodedRLP(consumed: (1 + length), result: result);
+  }
+
+  if (data[offset] >= 0xf8) {
+    final lengthLength = data[offset] - 0xf7;
+
+    final length = unarrayifyInteger(data, offset + 1, lengthLength);
+
+    return _decodeChildren(
+        data, offset, offset + 1 + lengthLength, lengthLength + length);
+  } else if (data[offset] >= 0xc0) {
+    final length = data[offset] - 0xc0;
+
+    return _decodeChildren(data, offset, offset + 1, length);
+  } else if (data[offset] >= 0xb8) {
+    final lengthLength = data[offset] - 0xb7;
+
+    final length = unarrayifyInteger(data, offset + 1, lengthLength);
+
+    final result = hex.encode(data.sublist(
+        offset + 1 + lengthLength, offset + 1 + lengthLength + length));
+    return DecodedRLP(consumed: (1 + lengthLength + length), result: result);
+  } else if (data[offset] >= 0x80) {
+    final length = data[offset] - 0x80;
+
+    final result = hex.encode(data.sublist(offset + 1, offset + 1 + length));
+    return DecodedRLP(consumed: (1 + length), result: result);
+  }
+
+  return DecodedRLP(consumed: 1, result: hexlifyByte(data[offset]));
+}
+
+/**
+ * @param {String} messageHex
+ * 
+ * This function takes a message hex string and returns a Transaction object.
+ * 
+ * @returns {Transaction}
+ */
+Transaction getTransactionFromMessageHash(String messageHex) {
+  final message = Uint8List.fromList(
+    hex.decode(
+      messageHex.replaceFirst("0x", ""),
+    ),
+  );
+
+  DecodedRLP en = decodeRLP(message, 0);
+
+  BigInt nonce = BigInt.parse(en.result[0], radix: 16);
+  BigInt gasPrice = BigInt.parse(en.result[1], radix: 16);
+  BigInt gasLimit = BigInt.parse(en.result[2], radix: 16);
+  String evmAddress = en.result[3];
+  BigInt value = BigInt.parse(en.result[4], radix: 16);
+
+  return Transaction(
+    nonce: nonce.toInt(),
+    gasPrice: EtherAmount.inWei(gasPrice),
+    maxGas: gasLimit.toInt(),
+    to: EthereumAddress.fromHex(evmAddress),
+    value: EtherAmount.inWei(value),
+  );
+}
--- test/no_ci/parse_messageHash_tx.dart
+++ test/no_ci/parse_messageHash_tx.dart
@@ -1,31 +1,19 @@
-import 'dart:convert';
 import 'dart:typed_data';
 import 'package:convert/convert.dart';
 import 'package:test/test.dart';
+import 'package:walletkit_dart/src/crypto/evm/rlp.dart';
+import 'package:web3dart/web3dart.dart';
 
 void main() {
   test('Parse a message hex to Transaction', () {
-    int bytesToInt(Uint8List bytes) {
-      int result = 0;
-      for (int byte in bytes) {
-        result = result * 256 + byte;
-      }
-      return result;
-    }
-
-    int decodeInt(Uint8List data, int index) {
-      if (index < data.length && data[index] <= 0x7f) {
-        return data[index];
-      } else {
-        int length = index < data.length ? data[index] - 0x80 : 0;
-        int endIndex = index + 1 + length;
-        endIndex = endIndex <= data.length ? endIndex : data.length;
-        return bytesToInt(data.sublist(index + 1, endIndex));
-      }
-    }
-
     const String messageHex =
         "0xf38207418502540be4008252089405870f1507d820212e921e1f39f14660336231d188016345785d8a0000808559454e49518080";
+
+    const String evmAddress = "05870f1507d820212e921e1f39f14660336231d1";
+    BigInt nonce = BigInt.from(1857);
+    BigInt gasPrice = BigInt.from(10000000000);
+    BigInt gasLimit = BigInt.from(21000);
+    BigInt value = BigInt.from(100000000000000000);
 
     final message = Uint8List.fromList(
       hex.decode(
@@ -33,21 +21,20 @@ void main() {
       ),
     );
 
-    int nonce = decodeInt(message, 1);
+    DecodedRLP en = decodeRLP(message, 0);
 
-    int gasPrice = decodeInt(message, 4);
+    BigInt nonceFromTx = BigInt.parse(en.result[0], radix: 16);
+    BigInt gasPriceFromTx = BigInt.parse(en.result[1], radix: 16);
+    BigInt gasLimitFromTx = BigInt.parse(en.result[2], radix: 16);
+    String evmAddressFromTx = en.result[3];
+    BigInt valueFromTx = BigInt.parse(en.result[4], radix: 16);
 
-    int gasLimit = decodeInt(message, 10);
-    // int toIndex = 6; // Set the correct index for "to" value
-    // int toLength = message[toIndex] - 0x80;
-    // int toEndIndex = toIndex + 1 + toLength;
-    // toEndIndex = toEndIndex <= message.length ? toEndIndex : message.length;
-    // Uint8List toBytes = message.sublist(toIndex + 1, toEndIndex);
-    // String toValue = hex.encode(toBytes);
-    // print("To: 0x$toValue");
+    Transaction tx = getTransactionFromMessageHash(messageHex);
 
-    expect(nonce, 1857);
-    expect(gasPrice, 10000000000);
-    expect(gasLimit, 21000);
+    expect(nonceFromTx, nonce);
+    expect(gasPriceFromTx, gasPrice);
+    expect(gasLimitFromTx, gasLimit);
+    expect(evmAddressFromTx, evmAddress);
+    expect(valueFromTx, value);
   });
 }
