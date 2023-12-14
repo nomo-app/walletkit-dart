ba56c23e0fb55e47d1e01b782fe2be96ecba87ab
dev2-nomo <147811533+dev2-nomo@users.noreply.github.com>
2023-12-14 10:07:05 +0100

    Regain simulation-tests that were lost during opensourcing (#14)
    * Regain simulation-tests that were lost during opensourcing
    
    * try to activate dotenv file
    
    * Update dart.yml
    
    * rename simulation_test.dart
    
    * move old wallet test into WalletKit
    
    * fix import
    
    ---------
    
    Co-authored-by: The Nomo App's core developer team <142992428+nomo-app@users.noreply.github.com>
    Co-authored-by: nomo-fe <dev0@nomo.app>

---
+++ test/ci/fetching/old_wallet_test.dart
@@ -0,0 +1,85 @@
+@Timeout(Duration(seconds: 600))
+import 'package:test/test.dart';
+import 'package:walletkit_dart/walletkit_dart.dart';
+import '../../no_ci/input_simulation_test.dart';
+
+void main() {
+  const ePubKeyNS =
+      "xpub683UNKpAnv1UrWKFVbkwQNT6Q9pKRbwBgcjfWRtnqmoScCE1XWN5i2SJRV2BciRphX2AW7o2YfCtxe5dr8QSXeuHp2A2SdF72VfnP5gn4UU";
+  const ePubKeyBip44LTC =
+      "xpub683UNKpAnv1UrPxMgQNgYqNm17KCq1CuZHHjKzxPQytUZgUii2mZnXX2ov2jbLbxVa9xH29fTChsLNyWDajCwTxxBx2DKEdd7Fan7GpGUVg";
+
+  const coinbaseTx =
+      "652a27f226ddfc74642fa4648de388985fc46bdcffd9f8d5f7ad5d4e5fd77944";
+
+  test(
+    'Fetch Zeniq Coinbase tx: $coinbaseTx',
+    () async {
+      final (tx, _) = await fetchUTXOTXByHash(
+        coinbaseTx,
+        ZeniqNetwork,
+        [],
+        [AddressType.legacy],
+      );
+      expect(tx.block, -1);
+      expect(tx.hash,
+          "652a27f226ddfc74642fa4648de388985fc46bdcffd9f8d5f7ad5d4e5fd77944");
+    },
+  );
+
+  test('Fetch Zeniq Txs $ePubKeyNS', () async {
+    final (txs, _) = await fetchUTXOTransactions(
+      ePubKey: ePubKeyNS,
+      walletTypes: [HDWalletType.NO_STRUCTURE],
+      addressTypes: [AddressType.legacy],
+      networkType: ZeniqNetwork,
+    );
+
+    expect(txs.length, greaterThanOrEqualTo(79));
+
+    expect(txs.whereType<NotAvaialableUTXOTransaction>().length, equals(0));
+
+    //reportCoinsAndAddresses(txList: txs, type: ZeniqNetwork);
+  });
+
+  test('Fetch Litecoin Txs $ePubKeyBip44LTC', () async {
+    final (txs, _) = await fetchUTXOTransactions(
+      ePubKey: ePubKeyBip44LTC,
+      walletTypes: [HDWalletType.BIP44_LITECOIN],
+      addressTypes: [AddressType.legacy, AddressType.segwit],
+      networkType: LitecoinNetwork,
+      minEndpoints: 1,
+    );
+
+    expect(txs.whereType<NotAvaialableUTXOTransaction>().length, equals(0));
+
+    expect(txs.length, greaterThanOrEqualTo(25));
+
+    //reportCoinsAndAddresses(txList: txs, type: LitecoinNetwork);
+  });
+
+  // This Test only works if computeUTXO Transactions fails for any tx
+  // test('Not Available Txs Cache Test', () async {
+  //   final (txs, nodes) = await fetchUTXOTransactions(
+  //     ePubKey: ePubKeyNS,
+  //     walletTypes: [HDWalletType.NO_STRUCTURE],
+  //     addressTypes: [AddressType.legacy],
+  //     networkType: ZeniqNetwork,
+  //   );
+
+  //   expect(txs.whereType<NotAvaialableUTXOTransaction>().length, equals(1));
+
+  //   final (txs2, _) = await fetchUTXOTransactions(
+  //     ePubKey: ePubKeyNS,
+  //     walletTypes: [HDWalletType.NO_STRUCTURE],
+  //     addressTypes: [AddressType.legacy],
+  //     networkType: ZeniqNetwork,
+  //     cachedNodes: nodes.toList(),
+  //     cachedTransactions: txs,
+  //   );
+
+  //   expect(txs2.whereType<NotAvaialableUTXOTransaction>().length, equals(1));
+
+  //   expect(txs2, equals(txs));
+  // });
+}
---
+++ test/no_ci/input_simulation_test.dart
@@ -0,0 +1,333 @@
+@Timeout(Duration(seconds: 600))
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:dotenv/dotenv.dart';
+import 'package:test/test.dart';
+import 'package:walletkit_dart/src/domain/entities/transactions/output.dart';
+import 'package:walletkit_dart/src/domain/entities/transactions/raw_transaction.dart';
+import 'package:walletkit_dart/src/domain/repository/endpoint_utils.dart';
+import 'package:walletkit_dart/walletkit_dart.dart';
+
+void main() {
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+
+  final rejectSeedString = env["REJECT_SEED"]!.split(",");
+  List<int> rejectIntList = rejectSeedString
+      .map((i) => int.parse(i))
+      .toList(); // Convert to list of integers
+  Uint8List rejectSeed = Uint8List.fromList(rejectIntList);
+
+  final spoilSeedString = env["SPOIL_SEED"]!.split(",");
+  List<int> spoilIntList = spoilSeedString
+      .map((i) => int.parse(i))
+      .toList(); // Convert to list of integers
+  Uint8List spoilSeed = Uint8List.fromList(spoilIntList);
+
+  test('Simulate All Send Zeniq Tx', () async {
+    final (txList, nodes) = await fetchUTXOTransactions(
+      seed: rejectSeed,
+      walletTypes: [HDWalletType.NO_STRUCTURE],
+      addressTypes: [AddressType.legacy],
+      networkType: ZeniqNetwork,
+      minEndpoints: 10,
+      maxLatency: Duration(
+        milliseconds: 500,
+      ),
+    );
+    expect(txList.length, greaterThanOrEqualTo(323));
+
+    final sendTxs = txList
+        .where((tx) =>
+            tx.transferMethod == TransactionTransferMethod.send ||
+            tx.transferMethod == TransactionTransferMethod.own)
+        .map((tx) => tx.id);
+
+    final results = await Future.wait([
+      for (final tx in sendTxs)
+        simulateTx(
+          hash: tx,
+          nodes: nodes,
+          seed: rejectSeed,
+          networkType: ZeniqNetwork,
+          addressTypes: [AddressType.legacy],
+        ),
+    ]);
+
+    //  final validSimulations = results.where((result) => result.$2).toList();
+    final invalidSimulations = results.where((result) => !result.$2).toList();
+
+    //  expect(validSimulations.length, greaterThanOrEqualTo(250));
+    expect(invalidSimulations, isEmpty);
+  });
+
+  test('Simulate BTC TXs rejectSeed', () async {
+    final addressTypes = [
+      AddressType.legacy,
+      AddressType.segwit,
+    ];
+    final (txList, nodes) = await fetchUTXOTransactions(
+      seed: rejectSeed,
+      walletTypes: [HDWalletType.NO_STRUCTURE],
+      addressTypes: addressTypes,
+      networkType: BitcoinNetwork,
+      minEndpoints: 10,
+      maxLatency: Duration(
+        milliseconds: 500,
+      ),
+    );
+    expect(txList.length, greaterThanOrEqualTo(74));
+    expect(
+      nodes.first.address,
+      startsWith("113G5SRkh8KqMfmqJKphuS1ALjxC3ead3Z"),
+    ); // just a sanity check
+
+    final sendTxs = txList
+        .where((tx) =>
+            tx.transferMethod == TransactionTransferMethod.send ||
+            tx.transferMethod == TransactionTransferMethod.own)
+        .map((tx) => tx.id);
+
+    final unknown = txList
+        .where((tx) => tx.transferMethod == TransactionTransferMethod.unknown)
+        .map((tx) => tx.id)
+        .toList();
+
+    expect(unknown, hasLength(0));
+
+    final results = await Future.wait([
+      for (final tx in sendTxs)
+        simulateTx(
+          hash: tx,
+          nodes: nodes,
+          seed: rejectSeed,
+          networkType: BitcoinNetwork,
+          addressTypes: addressTypes,
+        ),
+    ]);
+
+//    final validSimulations = results.where((result) => result.$2).toList();
+    final invalidSimulations = results.where((result) => !result.$2).toList();
+
+    //  expect(validSimulations.length, greaterThanOrEqualTo(62));
+    expect(invalidSimulations.length, 0);
+
+    for (final result in invalidSimulations) {
+      expect(
+        result.$1.inputs.any(
+            (element) => element.getAddress(BitcoinNetwork).startsWith("bc1")),
+        isTrue,
+      );
+    }
+  });
+
+  test('Litecoin Segwit Sending', () async {
+    final (txList, nodes) = await fetchUTXOTransactions(
+      seed: spoilSeed,
+      walletTypes: [HDWalletType.BIP44_LITECOIN],
+      addressTypes: [AddressType.segwit, AddressType.legacy],
+      networkType: LitecoinNetwork,
+      minEndpoints: 1,
+      maxLatency: Duration(
+        milliseconds: 500,
+      ),
+    );
+    expect(txList.length, greaterThanOrEqualTo(31));
+
+    final sendTxs = txList
+        .where((tx) =>
+            tx.transferMethod == TransactionTransferMethod.send ||
+            tx.transferMethod == TransactionTransferMethod.own)
+        .map((tx) => tx.id);
+
+    final results = await Future.wait([
+      for (final tx in sendTxs)
+        simulateTx(
+          hash: tx,
+          nodes: nodes,
+          seed: spoilSeed,
+          networkType: LitecoinNetwork,
+          addressTypes: [AddressType.segwit, AddressType.legacy],
+        ),
+    ]);
+
+    final validSimulations = results.where((result) => result.$2).toList();
+    final invalidSimulations = results.where((result) => !result.$2).toList();
+
+    expect(validSimulations.length, greaterThanOrEqualTo(15));
+    expect(invalidSimulations, isEmpty);
+  });
+
+  test('Bitcoincash Sending', () async {
+    final (txList, nodes) = await fetchUTXOTransactions(
+      seed: rejectSeed,
+      walletTypes: [HDWalletType.NO_STRUCTURE],
+      addressTypes: [AddressType.legacy, AddressType.cashaddr],
+      networkType: BitcoincashNetwork,
+      minEndpoints: 3,
+      maxLatency: Duration(
+        milliseconds: 800,
+      ),
+    );
+    // expect(txList.length, greaterThanOrEqualTo(17));
+
+    final sendTxs = txList
+        .where((tx) =>
+            tx.transferMethod == TransactionTransferMethod.send ||
+            tx.transferMethod == TransactionTransferMethod.own)
+        .map((tx) => tx.id);
+
+    final results = await Future.wait([
+      for (final tx in sendTxs)
+        simulateTx(
+          hash: tx,
+          nodes: nodes,
+          seed: rejectSeed,
+          networkType: BitcoincashNetwork,
+          addressTypes: [AddressType.segwit, AddressType.legacy],
+        ),
+    ]);
+
+    //  final validSimulations = results.where((result) => result.$2).toList();
+    final invalidSimulations = results.where((result) => !result.$2).toList();
+
+    //expect(validSimulations.length, greaterThanOrEqualTo(13));
+    expect(invalidSimulations, isEmpty);
+  });
+}
+
+///
+/// UTILS
+///
+Future<(UTXOTransaction, String?)> fetchUTXOTXByHash(
+  String hash,
+  UTXONetworkType networkType,
+  Iterable<NodeWithAddress> nodes,
+  Iterable<AddressType> addressTypes,
+) async {
+  final (result, _, _) = await fetchFromRandomElectrumXNode(
+    (client) async {
+      return (
+        await client.getTransactionForSimulation(
+          txHash: hash,
+          nodes: nodes,
+          type: networkType,
+          addressTypes: addressTypes,
+        ),
+        await client.getRaw(hash)
+      );
+    },
+    client: null,
+    endpoints: networkType.endpoints,
+    token: networkType.coin,
+  );
+
+  expect(result, isNotNull, reason: "Result is null for $hash");
+
+  return result!;
+}
+
+String buildTestTransactionWithOutputs({
+  required UTXONetworkType networkType,
+  required HDWalletType walletType,
+  required Map<ElectrumOutput, UTXOTransaction> chosenUTXOs,
+  required Uint8List seed,
+  required int version,
+  int fees = 1000,
+  required List<Output> outputs,
+}) {
+  const lockTime = 0;
+
+  final (_, inputMap) = buildInputs(chosenUTXOs);
+
+  ///
+  /// Build final transaction
+  ///
+
+  var tx = RawTransaction(
+    version: version,
+    lockTime: lockTime,
+    inputs: inputMap.values.toList(),
+    outputs: outputs,
+  );
+  final signedInputs = signInputs(
+    inputs: inputMap,
+    walletType: walletType,
+    tx: tx,
+    networkType: networkType,
+    seed: seed,
+  );
+  tx = tx.copyWith(inputs: signedInputs);
+
+  final serializedTx = tx.toHex();
+  return serializedTx;
+}
+
+Future<(UTXOTransaction, bool, String?)> simulateTx({
+  required String hash,
+  required Iterable<NodeWithAddress> nodes,
+  required Uint8List seed,
+  required UTXONetworkType networkType,
+  required Iterable<AddressType> addressTypes,
+  bool writeToFile = false,
+}) async {
+  // mainTx is an already confirmed transaction from the blockchain
+  final (expectedTx, expectedTxRaw) = await fetchUTXOTXByHash(
+    hash,
+    networkType,
+    nodes,
+    addressTypes,
+  );
+  try {
+    final usedUtxos = expectedTx.inputs
+        .where((input) => !input.isCoinbase)
+        .map((input) => (input.txid!, input.vout!));
+
+    final inputTxs = usedUtxos.map((utxo) async {
+      final (tx, _) = await fetchUTXOTXByHash(
+        utxo.$1,
+        networkType,
+        nodes,
+        addressTypes,
+      );
+      return (tx, tx.outputs[utxo.$2]);
+    });
+
+    final chosenUtxos = await Future.wait(inputTxs);
+    final chosenUtxosMap = {
+      for (final utxo in chosenUtxos) utxo.$2: utxo.$1,
+    };
+
+    final outputs = expectedTx.outputs
+        .map((out) => Output(
+            value: out.value, scriptPubKey: out.scriptPubKey.lockingScript))
+        .toList();
+
+    final fees = expectedTx.fee.value.toInt();
+
+    if (fees <= 0) {
+      return (expectedTx, false, "Fees <= 0");
+    }
+
+    final simulatedTx = buildTestTransactionWithOutputs(
+      networkType: networkType,
+      walletType: HDWalletType.NO_STRUCTURE,
+      chosenUTXOs: chosenUtxosMap,
+      seed: seed,
+      version: expectedTx.version,
+      fees: fees,
+      outputs: outputs,
+    );
+
+    if (writeToFile) {
+      final file = File("test/simulationOutput.txt");
+      await file.writeAsString(
+        "Simulation Tx\n$simulatedTx\n\nSource Tx\n$expectedTxRaw",
+        flush: true,
+      );
+    }
+
+    return (expectedTx, simulatedTx == expectedTxRaw, null);
+  } catch (e, s) {
+    return (expectedTx, false, "$e $s");
+  }
+}
