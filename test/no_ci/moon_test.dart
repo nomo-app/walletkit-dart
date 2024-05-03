f130ab8e146dcbf672231723038f07e7df6117e4
dev-aq <dev2@nomo.app>
2024-05-03 09:58:39 +0200

    add test for sending glmr

--- test/no_ci/moon_test.dart
+++ test/no_ci/moon_test.dart
@@ -4,20 +4,19 @@ import 'package:walletkit_dart/walletkit_dart.dart';
 import '../utils.dart';
 
 void main() {
-  //Test to send Arbitrum
-  test("Try to send Arbitrum", () async {
-    final arbitrumRPC = EvmRpcInterface(ArbitrumNetwork);
+  test("MoonBeam send test", () async {
+    final moonbeamRPC = EvmRpcInterface(MoonbeamNetwork);
 
     final intent = TransferIntent(
       recipient: arbitrumTestWallet,
       amount: Amount.convert(value: 0.001, decimals: 18),
       feeInfo: null,
-      token: arbitrum,
+      token: moonbeam,
       memo: null,
     );
     final testSeed = loadFromEnv("DEV_SEED");
 
-    final hash = await arbitrumRPC.sendERC20Token(
+    final hash = await moonbeamRPC.sendCoin(
       intent: intent,
       credentials: getETHCredentials(seed: testSeed),
     );
