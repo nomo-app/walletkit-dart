fe3403eb290aaa69e83befb741b5fab73a934e68
dev2-nomo <dev2@nomo.app>
2024-05-24 09:45:42 +0200

    refactor: Update rpcUrl in BASE_NETWORK

--- test/ci/evm/evm_rcp_test.dart
+++ test/ci/evm/evm_rcp_test.dart
@@ -115,19 +115,19 @@ void main() {
     expect(balance.value, greaterThanOrEqualTo(BigInt.from(0)));
   });
 
-  // test('Base Test', () async {
-  //   final rpcInterface = EvmRpcInterface(BaseNetwork);
+  test('Base Test', () async {
+    final rpcInterface = EvmRpcInterface(BaseNetwork);
 
-  //   final balance = await rpcInterface.fetchBalance(
-  //     address: arbitrumTestWallet,
-  //   );
-  //   expect(balance.value, greaterThanOrEqualTo(BigInt.from(0)));
+    final balance = await rpcInterface.fetchBalance(
+      address: arbitrumTestWallet,
+    );
+    expect(balance.value, greaterThanOrEqualTo(BigInt.from(0)));
 
-  //   final tokenBalance =
-  //       await rpcInterface.fetchTokenBalance(arbitrumTestWallet, mathToken);
+    final tokenBalance =
+        await rpcInterface.fetchTokenBalance(arbitrumTestWallet, mathToken);
 
-  //   expect(tokenBalance.value, greaterThanOrEqualTo(BigInt.from(0)));
-  // });
+    expect(tokenBalance.value, greaterThanOrEqualTo(BigInt.from(0)));
+  });
 
   test('MoonBeam Test', () async {
     final rpcInterface = EvmRpcInterface(MoonbeamNetwork);
